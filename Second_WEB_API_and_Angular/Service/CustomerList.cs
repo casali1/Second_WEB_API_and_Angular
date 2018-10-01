d3291947dfc3a485fc43601b592120360e104fa0
casali1 <frank_casali@hotmail.com>
2018-09-30 20:01:21 -0600

    More changes.

--- Second_WEB_API_and_Angular/Service/CustomerList.cs
+++ Second_WEB_API_and_Angular/Service/CustomerList.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using Second_WEB_API_and_Angular.Models;
 
 namespace Second_WEB_API_and_Angular.Service
